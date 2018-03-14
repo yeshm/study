6ee01f571e0a83e016e996ff01e881371fa25939
yeshm <yeshuangming@gmail.com>
2018-03-14 15:25:28 +0800

    使用ConfigMap配置demo-service host

--- kubernetes/spring-cloud-without-eureka/demo-client/src/main/java/org/yeshm/democlient/IDemoService.java
+++ kubernetes/spring-cloud-without-eureka/demo-client/src/main/java/org/yeshm/democlient/IDemoService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "demo-service", url = "http://demo-service:8111")
+@FeignClient(name = "demo-service", url = "http://${SPRING_DEMO_SERVICE_HOST}:9000")
 public interface IDemoService {
 
     @RequestMapping(method = RequestMethod.GET, value = "/test")
